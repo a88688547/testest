c8031712ee5bc97b7c0a4d3d50f4b86eb05f7dc2
a88688547 <a88688547@gmail.com>
2020-11-06 14:14:55 +0800

    1106.下午

---
+++ js/discount_scrollbar.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//     var controller = new ScrollMagic.Controller()
+
+//     var scroolbar1 = new ScrollMagic.Scene({
+//         triggerElement: '#activity',
+//         triggerHook: 0.85,
+//         // duration: `100%`,
+//     })
+//         .on('enter', function () {
+//             console.log('01')
+//             $('.activity_item').each(function () {
+//                 console.log('QQ')
+//                 $(this).delay(2000).show(1000)
+//             })
+//         })
+
+//         .addIndicators()
+//         .addTo(controller)
+// })
