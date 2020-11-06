f05868de7a6e368b4f8ac0467d4d9a35053c5e36
a88688547 <a88688547@gmail.com>
2020-11-06 09:04:24 +0800

    1106.早上

--- js/scroolbar.js
+++ js/scroolbar.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+    // -----------------------------------------------
+    // 點擊 側邊攔 會滾動
+    $("a[href^='#']").click(function (e) {
+        e.preventDefault()
+
+        var position = $($(this).attr('href')).offset().top
+
+        $('body, html').animate(
+            {
+                scrollTop: position - 80.8,
+            },
+            500
+        )
+        console.log(position)
+    })
+
+    // -----------------------------------------------
+    // -------------scrollmagic 效果------------------
     // var controller = new ScrollMagic.Controller()
     var controller = new ScrollMagic.Controller()
 
@@ -10,7 +28,7 @@ $(document).ready(function () {
             console.log('01')
         })
         .setClassToggle('#icon01', 'on')
-        .addIndicators()
+        // .addIndicators()
         .addTo(controller)
 
     var scroolbar2 = new ScrollMagic.Scene({
@@ -21,7 +39,7 @@ $(document).ready(function () {
             console.log('02')
         })
         .setClassToggle('#icon02', 'on')
-        .addIndicators()
+        // .addIndicators()
         .addTo(controller)
 
     var scroolbar2 = new ScrollMagic.Scene({
@@ -32,7 +50,7 @@ $(document).ready(function () {
             console.log('03')
         })
         .setClassToggle('#icon03', 'on')
-        .addIndicators()
+        // .addIndicators()
         .addTo(controller)
     var scroolbar2 = new ScrollMagic.Scene({
         triggerElement: '#key04',
@@ -42,7 +60,7 @@ $(document).ready(function () {
             console.log('04')
         })
         .setClassToggle('#icon04', 'on')
-        .addIndicators()
+        // .addIndicators()
         .addTo(controller)
     var scroolbar2 = new ScrollMagic.Scene({
         triggerElement: '#key05',
@@ -52,6 +70,73 @@ $(document).ready(function () {
             console.log('05')
         })
         .setClassToggle('#icon05', 'on')
-        .addIndicators()
+        // .addIndicators()
         .addTo(controller)
+    // --------------------------------------------------------------------
+    setInterval(resizecheck, 500) //設定定時器 每0.5秒做 高度確認
+
+    function resizecheck() {
+        var h1 = $('.section_1').height()
+        var h2 = $('.section_2').height()
+        var h3 = $('.section_3').height()
+        var h4 = $('.section_4').height()
+        var h5 = $('.section_5').height()
+
+        //   重新定義 scrollbar 設定
+
+        var controller = new ScrollMagic.Controller()
+
+        var scroolbar1 = new ScrollMagic.Scene({
+            triggerElement: '#key01',
+            duration: h1,
+        })
+            .on('enter', function () {
+                console.log('01')
+            })
+            .setClassToggle('#icon01', 'on')
+            // .addIndicators()
+            .addTo(controller)
+
+        var scroolbar2 = new ScrollMagic.Scene({
+            triggerElement: '#key02',
+            duration: h2,
+        })
+            .on('enter', function () {
+                console.log('02')
+            })
+            .setClassToggle('#icon02', 'on')
+            // .addIndicators()
+            .addTo(controller)
+
+        var scroolbar2 = new ScrollMagic.Scene({
+            triggerElement: '#key03',
+            duration: h3,
+        })
+            .on('enter', function () {
+                console.log('03')
+            })
+            .setClassToggle('#icon03', 'on')
+            // .addIndicators()
+            .addTo(controller)
+        var scroolbar2 = new ScrollMagic.Scene({
+            triggerElement: '#key04',
+            duration: h4,
+        })
+            .on('enter', function () {
+                console.log('04')
+            })
+            .setClassToggle('#icon04', 'on')
+            // .addIndicators()
+            .addTo(controller)
+        var scroolbar2 = new ScrollMagic.Scene({
+            triggerElement: '#key05',
+            duration: h5,
+        })
+            .on('enter', function () {
+                console.log('05')
+            })
+            .setClassToggle('#icon05', 'on')
+            // .addIndicators()
+            .addTo(controller)
+    }
 })
--- js/waterfall.js
+++ js/waterfall.js
@@ -1,4 +1,6 @@
 $(function () {
+    setInterval(checkResize, 500) //利用定時器 偵測寬度變化，0.5秒降低效能
+
     let width = $(window).width()
     if (width > 768) {
         //Trigger layout after each image loads and initialise Mansonry
@@ -22,9 +24,8 @@ $(function () {
         return false
     }
 
-    window.onresize = function () {
+    function checkResize() {
         let width = $(window).width()
-
         if (width > 768) {
             console.log('0 0')
             //Trigger layout after each image loads and initialise Mansonry
@@ -45,7 +46,37 @@ $(function () {
                     $('.loadingScreen').fadeOut()
                 })
         } else {
+            console.log('QQ')
             return false
         }
     }
+
+    // function checkResize() {
+    //     window.onresize = function () {
+    //         let width = $(window).width()
+
+    //         if (width >= 768) {
+    //             console.log('開啟瀑布流')
+    //             //Trigger layout after each image loads and initialise Mansonry
+    //             $('#waterfallArea').imagesLoaded(function () {
+    //                 $('#waterfallArea').masonry({
+    //                     itemSelector: '.content_box',
+    //                     columnWidth: 5, //gap
+    //                     animate: true,
+    //                     horizontalOrder: true,
+    //                     originTop: true,
+    //                 })
+    //             })
+
+    //             //Fade Out the loading screen when all images loaded
+    //             $('#waterfallArea')
+    //                 .imagesLoaded()
+    //                 .always(function (instance) {
+    //                     $('.loadingScreen').fadeOut()
+    //                 })
+    //         } else {
+    //             return false
+    //         }
+    //     }
+    // }
 })
